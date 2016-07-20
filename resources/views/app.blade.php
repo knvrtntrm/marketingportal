fb2e6e9970a1c1084ddadd5090cf4ef291fcc9b8
webdesigner-konvert <webdesigner@konvert.be>
2016-07-20 10:00:13 +0200

    Restyle

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -138,7 +138,7 @@
             <!-- Sidebar user panel -->
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="images/states/1.png" class="img-circle" alt="User Image">
+                    <img src="images/K.jpg" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
                     <p>{{ Auth::user()->name }}</p>
